44e332a03fb63ade99e04ded8e216c317931a73b
Sleyther Calsin <sleyther.calsin@cloudcomputing.com.pe>
2023-11-10 15:22:23 -0500

    First Commit

---
+++ calificacion-service/src/main/java/com/msvc/calificacion/controller/CalificacionController.java
@@ -0,0 +1,41 @@
+package com.msvc.calificacion.controller;
+
+import com.msvc.calificacion.entity.Calificacion;
+import com.msvc.calificacion.services.CalificacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calificaciones")
+public class CalificacionController {
+
+    @Autowired
+    private CalificacionService calificacionService;
+
+    @PostMapping("/")
+    public ResponseEntity<Calificacion> guardarCalificacion(@RequestBody Calificacion calificacion) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(calificacionService.create(calificacion));
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Calificacion>> listarCalificaciones() {
+        return ResponseEntity.ok().body(calificacionService.getCalificaciones());
+    }
+
+    @GetMapping("/usuarios/{usuarioId}")
+    public ResponseEntity<List<Calificacion>> listarCalificacionesPorUsuarioId(@PathVariable String usuarioId) {
+        return ResponseEntity.ok().body(calificacionService.getCalificacionesByUsuarioId(usuarioId));
+    }
+
+    @GetMapping("/hoteles/{hotelId}")
+    public ResponseEntity<List<Calificacion>> listarCalificacionesPorHotelId(@PathVariable String hotelId) {
+        return ResponseEntity.ok().body(calificacionService.getCalificacionesByHotelId(hotelId));
+    }
+
+
+}
